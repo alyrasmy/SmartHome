445f4905d3ac71d1a02350fb46ad415e0a973758
Ubuntu <ubuntu@ip-172-31-63-110.ec2.internal>
2017-02-21 04:09:09 +0000

    fixed issue with new attributes added

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -11,8 +11,8 @@ import time
 if __name__ == "__main__":
     schedule.every().day.at("23:58").do(jobs.led_job);
     schedule.every().day.at("23:59").do(jobs.resetLedTimer);
-    schedule.every(10).minutes.do(jobs.temperature_job);
-    schedule.every(10).minutes.do(jobs.humidity_job);
+    schedule.every(60).minutes.do(jobs.temperature_job);
+    schedule.every(60).minutes.do(jobs.humidity_job);
     while True:
-    	schedule.run_pending();
- 	time.sleep(1);
+        schedule.run_pending();
+        time.sleep(1);
